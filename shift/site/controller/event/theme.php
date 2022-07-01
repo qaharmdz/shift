ee088c227b501615c564522f290ab587a9ee1f8d
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-07-01 22:54:09 +0700

    Temp fix default theme

--- shift/site/controller/event/theme.php
+++ shift/site/controller/event/theme.php
@@ -10,16 +10,16 @@ class Theme extends Mvc\Controller
 {
     public function index(&$view, &$data, &$output)
     {
-        if (!$this->config->get($this->config->get('config_theme') . '_status')) {
-            exit('Error: A theme has not been assigned to this store!');
-        }
+        // if (!$this->config->get($this->config->get('config_theme') . '_status')) {
+        //     exit('Error: A theme has not been assigned to this store!');
+        // }
 
         // This is only here for compatibility with older extensions
         // if (substr($view, -3) == 'tpl') {
         //     $view = substr($view, 0, -3);
         // }
 
-        if ($this->config->get('config_theme') == 'theme_default') {
+        if ($this->config->get('config_theme') == 'theme_default' || $this->config->get('config_theme') == 'themedefault') {
             $theme = $this->config->get('theme_default_directory');
         } else {
             $theme = $this->config->get('config_theme');
