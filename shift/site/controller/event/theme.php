a22a489acb295a677121ba1238d6f87f9cfb1255
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-07-17 20:35:08 +0700

    Chore cleanup event/theme

--- shift/site/controller/event/theme.php
+++ shift/site/controller/event/theme.php
@@ -10,20 +10,7 @@ class Theme extends Mvc\Controller
 {
     public function index(&$view, &$data, &$output)
     {
-        // if (!$this->config->get($this->config->get('system.setting.theme') . '_status')) {
-        //     exit('Error: A theme has not been assigned to this site!');
-        // }
-
-        // This is only here for compatibility with older extensions
-        // if (substr($view, -3) == 'tpl') {
-        //     $view = substr($view, 0, -3);
-        // }
-
-        if ($this->config->get('system.setting.theme') == 'theme_default' || $this->config->get('system.setting.theme') == 'themedefault') {
-            $theme = $this->config->get('theme_default_directory');
-        } else {
-            $theme = $this->config->get('system.setting.theme');
-        }
+        $theme = $this->config->get('system.site.theme', 'default');
 
         if (is_file(DIR_TEMPLATE . $theme . '/template/' . $view . '.tpl')) {
             $view = $theme . '/template/' . $view;
