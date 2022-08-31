9745059f6364d87d11189add4eac5ff4ce570734
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-08-31 19:55:38 +0700

    Fixed startup/router order

--- shift/system/config/app/admin.php
+++ shift/system/config/app/admin.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 $_['app_startup'] = [
     'startup/startup',
+    'startup/router',
     'startup/event',
     'startup/login',
     'startup/permission',
-    'startup/router',
 ];
 
 $_['action_default'] = 'page/dashboard';
