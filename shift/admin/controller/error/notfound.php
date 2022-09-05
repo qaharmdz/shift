edcbc631e6f13196762df45b2200a83420a147e2
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-09-06 06:16:21 +0700

    Fixed route error/notfound

--- shift/admin/controller/error/notfound.php
+++ shift/admin/controller/error/notfound.php
@@ -12,28 +12,19 @@ class NotFound extends Mvc\Controller
     {
         $this->load->language('error/notfound');
 
-        $this->document->setTitle($this->language->get('heading_title'));
+        $this->document->setTitle($this->language->get('page_title'));
 
-        $data['heading_title'] = $this->language->get('heading_title');
+        $this->document->addNode('class_body', ['error-404']);
+        $this->document->addNode('breadcrumbs', [
+            [$this->language->get('page_title'), $this->router->url($this->request->get('query.route'))],
+        ]);
 
-        $data['text_not_found'] = $this->language->get('text_not_found');
+        $data = [];
 
-        $data['breadcrumbs'] = array();
+        $data['layouts'] = $this->load->controller('block/position');
+        $data['footer']  = $this->load->controller('block/footer');
+        $data['header']  = $this->load->controller('block/header');
 
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->router->url('common/dashboard')
-        );
-
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->router->url('error/not_found')
-        );
-
-        $data['header'] = $this->load->controller('common/header');
-        $data['column_left'] = $this->load->controller('common/column_left');
-        $data['footer'] = $this->load->controller('common/footer');
-
-        $this->response->setOutput($this->load->view('error/not_found', $data));
+        $this->response->setOutput($this->load->view('error/notfound', $data));
     }
 }
