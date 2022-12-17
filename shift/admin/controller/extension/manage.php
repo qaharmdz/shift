de3702fe7817cdad248c27f39ec52670c0fa94d5
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-12-17 10:01:51 +0700

    Fixed sketsa list

--- shift/admin/controller/extension/manage.php
+++ shift/admin/controller/extension/manage.php
@@ -19,6 +19,20 @@ class Manage extends Mvc\Controller
             [$this->language->get('page_title')],
         ]);
 
+        $extensions = [];
+        foreach (glob(PATH_EXTENSIONS . '*', GLOB_ONLYDIR) as $ext) {
+            $extensions[basename($ext)] = [];
+
+            foreach (glob($ext . DS . '*', GLOB_ONLYDIR | GLOB_NOESCAPE) as $node) {
+                $extensions[basename($ext)][] = [
+                    'codename' => basename($node),
+                    'path'     => $node,
+                ];
+            }
+        }
+
+        d($extensions);
+
         $data = [];
 
         $data['layouts'] = $this->load->controller('block/position');
