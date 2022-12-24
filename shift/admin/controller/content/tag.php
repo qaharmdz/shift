91425edf9e8fd1ef4b94212179ad73e33cefb8a5
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-12-24 16:01:35 +0700

    Added content/tag list

---
+++ shift/admin/controller/content/tag.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shift\Admin\Controller\Content;
+
+use Shift\System\Mvc;
+
+class Tag extends Mvc\Controller
+{
+    public function index()
+    {
+        $this->load->language('content/tag');
+
+        $this->document->setTitle($this->language->get('page_title'));
+
+        $this->document->loadAsset('datatables');
+
+        $this->document->addNode('breadcrumbs', [
+            [$this->language->get('content')],
+            [$this->language->get('page_title'), $this->router->url('content/tag')],
+        ]);
+
+        $data = [];
+
+        $data['layouts'] = $this->load->controller('block/position');
+        $data['footer']  = $this->load->controller('block/footer');
+        $data['header']  = $this->load->controller('block/header');
+
+        $this->response->setOutput($this->load->view('content/tag_list', $data));
+    }
+
+    public function list()
+    {
+        if (!$this->request->has('post.draw')) {
+            return $this->response->setOutputJson($this->language->get('error_precondition'), 412);
+        }
+
+        $this->load->model('content/tag');
+
+        $params  = $this->request->get('post');
+        $results = $this->model_content_tag->dtRecords($params);
+
+        $items = [];
+        for ($i = 0; $i < $results->num_rows; $i++) {
+            $items[$i] = $results->rows[$i];
+
+            $items[$i]['DT_RowClass'] = 'dt-row-' . $items[$i]['tag_id'];
+            $items[$i]['url_edit']    = $this->router->url('content/tag', 'tag_id=' . $items[$i]['tag_id']);
+        }
+
+        $data = [
+            'draw' => (int)$params['draw'] ?? 1,
+            'data' => $items,
+            'recordsFiltered' => $results->num_rows,
+            'recordsTotal'    => $this->model_content_tag->getTotal(),
+        ];
+
+        $this->response->setOutputJson($data);
+    }
+
+    public function dtaction()
+    {
+        $this->load->model('content/tag');
+        $this->load->language('content/tag');
+
+        if (!$this->user->hasPermission('modify', 'content/tag')) {
+            return $this->response->setOutputJson($this->language->get('error_permission'), 403);
+        }
+        if (!$this->request->is(['post', 'ajax'])) {
+            return $this->response->setOutputJson($this->language->get('error_request_method'), 405);
+        }
+
+        $post  = array_replace(['type' => '', 'item' => ''], $this->request->get('post'));
+        $types = ['enabled', 'disabled', 'delete'];
+        $items = explode(',', $post['item']);
+        $data  = [
+            'items'     => $items,
+            'message'   => '',
+            'updated'   => [],
+        ];
+
+        if (empty($items) || !in_array($post['type'], $types) || in_array(0, $items)) {
+            return $this->response->setOutputJson($this->language->get('error_precondition'), 412);
+        }
+
+        $data['updated'] = $this->model_content_tag->dtAction($post['type'], $items);
+        $data['message'] = $post['message'] ?? $this->language->get('success_' . $post['type']);
+
+        $this->response->setOutputJson($data);
+    }
+}
---
+++ shift/admin/model/content/tag.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shift\Admin\Model\Content;
+
+use Shift\System\Mvc;
+use Shift\System\Helper;
+
+class Tag extends Mvc\Model
+{
+
+    // List
+    // ================================================
+
+    /**
+     * DataTables records
+     *
+     * @param  array  $params
+     */
+    public function dtRecords(array $params)
+    {
+        $columnMap = [
+            'category_id' => 't.term_id AS tag_id',
+            'title'       => 'tc.title',
+            'alias'       => 'ra.alias',
+            'order'       => 't.sort_order AS `order`',
+            'status'      => 't.status',
+            'created'     => 't.created',
+            'updated'     => 't.updated'
+        ];
+        $filterMap = $columnMap;
+        $filterMap['category_id'] = 't.term_id';
+        $filterMap['title']       = 'CONCAT(tc.title, " ", ra.alias)';
+
+        $dtResult  = Helper\DataTables::parse($params, $filterMap);
+
+        $query = "SELECT " . implode(', ', $columnMap)
+            . " FROM `" . DB_PREFIX . "term` t
+                LEFT JOIN `" . DB_PREFIX . "term_content` tc ON (tc.term_id = t.term_id AND tc.language_id = " . $this->config->getInt('env.language_id') . ")
+                LEFT JOIN `" . DB_PREFIX . "route_alias` ra ON (ra.param = 'category_id' AND ra.value = t.term_id AND ra.language_id = " . $this->config->getInt('env.language_id') . ")"
+            . " WHERE t.`taxonomy` = 'post_tag'"
+                 . ($dtResult['query']['where'] ? " AND " . $dtResult['query']['where'] : "")
+            . " ORDER BY " . $dtResult['query']['order']
+            . " LIMIT " . $dtResult['query']['limit'];
+
+        return $this->db->get($query, $dtResult['query']['params']);
+    }
+
+    public function dtAction(string $type, array $items): array
+    {
+        $_items = [];
+
+        if (in_array($type, ['enabled', 'disabled'])) {
+            $status = $type == 'enabled' ? 1 : 0;
+
+            foreach ($items as $item) {
+                $this->db->set(
+                    DB_PREFIX . 'term',
+                    [
+                        'status'  => $status,
+                        'updated' => date('Y-m-d H:i:s'),
+                    ],
+                    [
+                        'term_id'  => (int)$item,
+                        'taxonomy' => 'post_tag',
+                    ]
+                );
+
+                if ($this->db->affectedRows()) {
+                    $_items[] = $item;
+                }
+            }
+        }
+
+        if ($type == 'delete') {
+            $this->deleteTags($items);
+        }
+
+        return $_items;
+    }
+
+    // Form CRUD
+    // ================================================
+
+    public function getTag(int $category_id): array
+    {
+        //
+    }
+
+    public function getTotal(): int
+    {
+        return $this->db->get("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "term` WHERE `taxonomy` = 'post_category'")->row['total'];
+    }
+
+    public function deleteTags(array $tags): void
+    {
+        $this->db->delete(DB_PREFIX . 'term', ['term_id' => $tags]);
+        $this->db->delete(DB_PREFIX . 'term_content', ['term_id' => $tags]);
+        $this->db->delete(DB_PREFIX . 'term_meta', ['term_id' => $tags]);
+        $this->db->delete(DB_PREFIX . 'term_relation', ['term_id' => $tags]);
+
+        $this->cache->delete('tags');
+    }
+}
