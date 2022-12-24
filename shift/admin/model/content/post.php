5e6facb8289d50891a48e68a6ddc01572855eb42
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2022-12-24 08:08:48 +0700

    Added route alias to content/post list model

--- shift/admin/model/content/post.php
+++ shift/admin/model/content/post.php
@@ -23,6 +23,7 @@ class Post extends Mvc\Model
         $columnMap = [
             'post_id'   => 'p.post_id',
             'title'     => 'pc.title',
+            'alias'     => 'ra.alias',
             'category'  => 'tc.title AS category',
             'author'    => 'CONCAT(u.firstname, " ", u.lastname) AS author',
             'status'    => 'p.status',
@@ -32,13 +33,14 @@ class Post extends Mvc\Model
             'unpublish' => 'p.unpublish',
         ];
         $filterMap = $columnMap;
+        $filterMap['post_id'] = 'ed.extension_data_id';
 
         $dtResult  = Helper\DataTables::parse($params, $filterMap);
-        $filterMap['post_id'] = 'ed.extension_data_id';
 
         $query = "SELECT " . implode(', ', $columnMap)
             . " FROM `" . DB_PREFIX . "post` p
                 LEFT JOIN `" . DB_PREFIX . "post_content` pc ON (pc.post_id = p.post_id AND pc.language_id = " . $this->config->getInt('env.language_id') . ")
+                LEFT JOIN `" . DB_PREFIX . "route_alias` ra ON (ra.param = 'post_id' AND ra.value = p.post_id AND ra.language_id = " . $this->config->getInt('env.language_id') . ")
                 LEFT JOIN `" . DB_PREFIX . "term` t ON (t.term_id = p.term_id)
                 LEFT JOIN `" . DB_PREFIX . "term_content` tc ON (tc.term_id = p.term_id)
                 LEFT JOIN `" . DB_PREFIX . "user` u ON (u.user_id = p.user_id)"
@@ -95,9 +97,9 @@ class Post extends Mvc\Model
 
     public function deletePosts(array $posts): void
     {
-        $this->db->delete(DB_PREFIX . 'post', ['post_id' => $users]);
-        $this->db->delete(DB_PREFIX . 'post_content', ['post_id' => $users]);
-        $this->db->delete(DB_PREFIX . 'post_meta', ['post_id' => $users]);
+        $this->db->delete(DB_PREFIX . 'post', ['post_id' => $posts]);
+        $this->db->delete(DB_PREFIX . 'post_content', ['post_id' => $posts]);
+        $this->db->delete(DB_PREFIX . 'post_meta', ['post_id' => $posts]);
 
         $this->cache->delete('posts');
     }
