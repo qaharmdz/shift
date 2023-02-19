1ad03d194cb2149cecfff2bfafdd65025a016556
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-02-19 16:12:55 +0700

    Added content/setting save

--- shift/admin/controller/content/setting.php
+++ shift/admin/controller/content/setting.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shift\Admin\Controller\Content;
 
 use Shift\System\Mvc;
+use Shift\System\Helper\Arr;
 
 class Setting extends Mvc\Controller
 {
@@ -46,9 +47,8 @@ class Setting extends Mvc\Controller
 
     public function save()
     {
-        /*
         $this->load->config('content/setting');
-        $this->load->model('content/setting');
+        $this->load->model('setting/setting');
         $this->load->language('content/setting');
 
         if (!$this->user->hasPermission('modify', 'content/setting')) {
@@ -57,89 +57,31 @@ class Setting extends Mvc\Controller
         if (!$this->request->is(['post', 'ajax'])) {
             return $this->response->setOutputJson($this->language->get('error_request_method'), 405);
         }
-        if (!$this->request->has('post.tag_id')) {
+        if (!$this->request->has('post.site_id')) {
             return $this->response->setOutputJson($this->language->get('error_precondition'), 412);
         }
 
         $data = [];
         $post = array_replace_recursive(
-            $this->config->getArray('content.category.form'),
+            $this->config->getArray('content.setting.form'),
             $this->request->get('post', [])
         );
-        $tag_id = (int)$post['tag_id'];
-
-        unset($post['content'][0]);
+        $site_id = (int)$post['site_id'];
 
         if ($errors = $this->validate($post)) {
             return $this->response->setOutputJson($errors, 422);
         }
 
-        if (!$tag_id) {
-            // $data['new_id'] = $this->model_content_setting->addTag($post);
-        } else {
-            // $this->model_content_setting->editTag($tag_id, $post);
-        }
+        Arr::unset($post, ['site_id', 'form', 'action', 'timezone']);
 
-        // Redirect
-        if ($post['action'] === 'close') {
-            $data['redirect'] = $this->router->url('content/setting');
-        }
-        if ($post['action'] === 'new') {
-            $data['redirect'] = $this->router->url('content/setting/form');
-        }
-        if (isset($data['new_id']) && empty($data['redirect'])) {
-            $data['redirect'] = $this->router->url('content/setting/form', 'tag_id=' . $data['new_id']);
-        }
-        */
+        $this->model_setting_setting->editSetting('content', 'setting', $post, $site_id);
 
-        $data = [];
         $this->response->setOutputJson($data);
     }
 
     protected function validate(array $post): array
     {
         $errors = [];
-
-        foreach ($post['content'] as $language_id => $content) {
-            if (!$this->assert->lengthBetween(2, 200)->check($content['title'])) {
-                $errors['items']['content[' . $language_id . '][title]'] = sprintf($this->language->get('error_length_between'), 2, 200);
-            }
-        }
-
-        foreach ($post['alias'] as $language_id => &$alias) {
-            if (!$alias = str_replace(' ', '-', trim($alias))) {
-                continue;
-            }
-
-            if (count(array_keys($post['alias'], $alias)) > 1) {
-                $errors['items']['alias[' . $language_id . ']'] = $this->language->get('error_alias_unique');
-            }
-
-            if (empty($errors['items']['alias[' . $language_id . ']'])) {
-                $aliases = $this->db->get(
-                    "SELECT * FROM `" . DB_PREFIX . "route_alias` WHERE `language_id` != ?i AND `alias` = ?s",
-                    [$language_id, $alias]
-                )->rows;
-
-                foreach ($aliases as $alias) {
-                    if (
-                        !$post['tag_id']
-                        || (
-                            $alias['route'] == 'content/setting'
-                            && $alias['param'] == 'tag_id'
-                            && $alias['value'] != $post['tag_id']
-                        )
-                    ) {
-                        $errors['items']['alias[' . $language_id . ']'] = $this->language->get('error_alias_exist');
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (isset($errors['items'])) {
-            $errors['response'] = $this->language->get('error_form');
-        }
 
         return $errors;
     }
