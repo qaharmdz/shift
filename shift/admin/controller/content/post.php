84c98037a3a409eac3eee667509edde895807212
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-02-25 21:43:45 +0700

    Added content/post form select2

--- shift/admin/controller/content/post.php
+++ shift/admin/controller/content/post.php
@@ -110,6 +110,7 @@ class Post extends Mvc\Controller
         $this->document->setTitle($this->language->get('page_title'));
 
         $this->document->loadAsset('editor');
+        $this->document->loadAsset('select2');
         $this->document->loadAsset('flatpickr');
 
         $this->document->addNode('breadcrumbs', [
--- shift/admin/language/en/content/post.php
+++ shift/admin/language/en/content/post.php
@@ -1,17 +1,18 @@
 <?php
 
-$_['page_title']        = 'Posts';
+$_['page_title']         = 'Posts';
 
-$_['excerpt']           = 'Excerpt';
-$_['default_category']  = 'Default Category';
-$_['categories']        = 'Categories';
-$_['visibility']        = 'Visibility';
+$_['excerpt']            = 'Excerpt';
+$_['primary_category']   = 'Primary Category';
+$_['categories']         = 'Categories';
+$_['tags']               = 'Tags';
+$_['visibility']         = 'Visibility';
 
-$_['draft']             = 'Draft';
-$_['pending']           = 'Pending';
-$_['trash']             = 'Trash';
-$_['usergroup']         = 'User Groups';
-$_['password']          = 'Password';
+$_['draft']              = 'Draft';
+$_['pending']            = 'Pending';
+$_['trash']              = 'Trash';
+$_['usergroup']          = 'User Groups';
+$_['password']           = 'Password';
 
 $_['error_alias_exist']  = 'URL alias keyword already in use!';
 $_['error_alias_unique'] = 'URL alias must be unique for each language!';
