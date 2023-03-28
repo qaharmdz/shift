93ed2b156cbc9d1c3f8a549c4ddcf74b14bf9766
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-03-29 01:34:32 +0700

    Added asset jstree

--- shift/admin/controller/startup/asset.php
+++ shift/admin/controller/startup/asset.php
@@ -56,13 +56,18 @@ class Asset extends Mvc\Controller
         $this->document->addAsset('select2', [
             'style'  => [
                 $this->config->get('env.url_app') . 'asset/script/select2/select2.min.css',
-                // 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css'
-                // 'https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css'
             ],
             'script' => [
                 $this->config->get('env.url_app') . 'asset/script/select2/select2.min.js',
-                // 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js'
-                // 'https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js'
+            ]
+        ]);
+
+        $this->document->addAsset('jstree', [
+            'style'     => [
+                $this->config->get('env.url_app') . 'asset/script/jstree/themes/style.min.css'
+            ],
+            'script'    => [
+                $this->config->get('env.url_app') . 'asset/script/jstree/jstree.min.js'
             ]
         ]);
     }
