8f036c31f918169a8429ecd945e550de5b3634c7
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-04-06 04:07:16 +0700

    Fixed media manager modal tooltip

--- shift/admin/language/en/tool/mediamanager.php
+++ shift/admin/language/en/tool/mediamanager.php
@@ -8,5 +8,6 @@ $_['reload_tree']       = 'Reload folders';
 $_['new_folder']        = 'New Folder';
 $_['rename']            = 'Rename';
 $_['select']            = 'Select';
+$_['select_image']      = 'Select Image';
 
 $_['empty_folder']      = 'Folder is empty';
