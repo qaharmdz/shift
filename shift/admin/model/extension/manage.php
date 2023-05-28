46701484c4b5e82bf55cd3b5f554bc7a7bcac4a7
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-05-28 17:09:55 +0700

    Fixed model extensions/manage getInstalled db param

--- shift/admin/model/extension/manage.php
+++ shift/admin/model/extension/manage.php
@@ -93,7 +93,7 @@ class Manage extends Mvc\Model
     public function getInstalled(string $type)
     {
         $data = [];
-        $query = $this->db->get("SELECT * FROM `" . DB_PREFIX . "extension` WHERE `type` = ?s ORDER BY codename", $type);
+        $query = $this->db->get("SELECT * FROM `" . DB_PREFIX . "extension` WHERE `type` = ?s ORDER BY codename", [$type]);
 
         foreach ($query->rows as $result) {
             $data[] = $result['codename'];
