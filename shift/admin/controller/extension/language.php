035fc8e60189d9f733b7329d837383aa5eda52b6
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-08-13 17:43:46 +0700

    Added extension/language list

--- shift/admin/controller/extension/language.php
+++ shift/admin/controller/extension/language.php
@@ -14,6 +14,8 @@ class Language extends Mvc\Controller
 
         $this->document->setTitle($this->language->get('page_title'));
 
+        $this->document->loadAsset('datatables');
+
         $this->document->addNode('breadcrumbs', [
             [$this->language->get('extensions')],
             [$this->language->get('page_title')],
@@ -26,5 +28,34 @@ class Language extends Mvc\Controller
         $data['header']  = $this->load->controller('block/header');
 
         $this->response->setOutput($this->load->view('extension/language', $data));
+    }
+
+    public function list()
+    {
+        if (!$this->request->has('post.draw')) {
+            return $this->response->setOutputJson($this->language->get('error_precondition'), 412);
+        }
+
+        $this->load->model('extension/language');
+
+        $params  = $this->request->get('post');
+        $results = $this->model_extension_language->dtRecords($params);
+
+        $items = [];
+        for ($i = 0; $i < $results->num_rows; $i++) {
+            $items[$i] = $results->rows[$i];
+
+            $items[$i]['DT_RowClass'] = 'dt-row-' . $items[$i]['extension_id'];
+            $items[$i]['url_edit']    = $this->router->url('extension/language/form', 'extension_id=' . $items[$i]['extension_id']);
+        }
+
+        $data = [
+            'draw' => (int)$params['draw'] ?? 1,
+            'data' => $items,
+            'recordsFiltered' => $results->num_rows,
+            'recordsTotal'    => $this->model_extension_language->getTotal(),
+        ];
+
+        $this->response->setOutputJson($data);
     }
 }
--- shift/admin/model/extension/language.php
+++ shift/admin/model/extension/language.php
@@ -5,9 +5,68 @@ declare(strict_types=1);
 namespace Shift\Admin\Model\Extension;
 
 use Shift\System\Mvc;
+use Shift\System\Helper;
 
 class Language extends Mvc\Model
 {
+    /**
+     * DataTables records
+     *
+     * @param  array  $params
+     */
+    public function dtRecords(array $params): \stdClass
+    {
+        $columnMap = [
+            'extension_id' => 'e.extension_id',
+            'codename'     => 'e.codename',
+            'name'         => 'e.name',
+            'status'       => 'e.status',
+        ];
+        $filterMap = $columnMap;
+        $dtResult  = Helper\DataTables::parse($params, $filterMap);
+
+        $query = "SELECT " . implode(', ', $columnMap)
+            . " FROM `" . DB_PREFIX . "extension` e"
+            . " WHERE e.`type` = 'language' AND e.`install` = 1"
+                . ($dtResult['query']['where'] ? " AND  " . $dtResult['query']['where'] : "")
+            . " ORDER BY " . $dtResult['query']['order']
+            . " LIMIT " . $dtResult['query']['limit'];
+
+        return $this->db->get($query, $dtResult['query']['params']);
+    }
+
+    public function dtAction(string $type, array $items): array
+    {
+        $_items = [];
+
+        if (in_array($type, ['enabled', 'disabled'])) {
+            $status = $type == 'enabled' ? 1 : 0;
+
+            $this->db->set(
+                DB_PREFIX . 'extension',
+                [
+                    'status'  => $status,
+                    'updated' => date('Y-m-d H:i:s'),
+                ],
+                ['extension_id' => $items]
+            );
+
+            if ($this->db->affectedRows()) {
+                $_items[] = $items;
+            }
+        }
+
+        return $_items;
+    }
+
+    public function getTotal(): int
+    {
+        return (int)$this->db->get(
+            "SELECT COUNT(*) AS total
+            FROM `" . DB_PREFIX . "extension`
+            WHERE `type` = 'language'  AND `status` = 1 AND `install` = 1"
+        )->row['total'];
+    }
     /**
      * Get languages
      *
@@ -43,117 +102,4 @@ class Language extends Mvc\Model
 
         return $data;
     }
-
-    /*
-    public function addLanguage($data)
-    {
-        $this->db->query("INSERT INTO " . DB_PREFIX . "language SET name = '" . $this->db->escape($data['name']) . "', code = '" . $this->db->escape($data['code']) . "', locale = '" . $this->db->escape($data['locale']) . "', sort_order = '" . $this->db->escape($data['sort_order']) . "', status = '" . (int)$data['status'] . "'");
-
-        $this->cache->deleteByTags('languages');
-
-        $language_id = $this->db->getLastId();
-
-        // Banner
-        $query = $this->db->get("SELECT * FROM " . DB_PREFIX . "banner_image WHERE language_id = '" . (int)$this->config->get('env.language_id') . "'");
-
-        foreach ($query->rows as $banner_image) {
-            $this->db->query("INSERT INTO " . DB_PREFIX . "banner_image SET banner_id = '" . (int)$banner_image['banner_id'] . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($banner_image['title']) . "', link = '" . $this->db->escape($banner_image['link']) . "', image = '" . $this->db->escape($banner_image['image']) . "', sort_order = '" . (int)$banner_image['sort_order'] . "'");
-        }
-
-        $this->cache->deleteByTags('banners');
-
-        // Download
-        $query = $this->db->get("SELECT * FROM " . DB_PREFIX . "download_description WHERE language_id = '" . (int)$this->config->get('env.language_id') . "'");
-
-        foreach ($query->rows as $download) {
-            $this->db->query("INSERT INTO " . DB_PREFIX . "download_description SET download_id = '" . (int)$download['download_id'] . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($download['name']) . "'");
-        }
-
-        // Information
-        $query = $this->db->get("SELECT * FROM " . DB_PREFIX . "information_description WHERE language_id = '" . (int)$this->config->get('env.language_id') . "'");
-
-        foreach ($query->rows as $information) {
-            $this->db->query("INSERT INTO " . DB_PREFIX . "information_description SET information_id = '" . (int)$information['information_id'] . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($information['title']) . "', description = '" . $this->db->escape($information['description']) . "', meta_title = '" . $this->db->escape($information['meta_title']) . "', meta_description = '" . $this->db->escape($information['meta_description']) . "', meta_keyword = '" . $this->db->escape($information['meta_keyword']) . "'");
-        }
-
-        $this->cache->deleteByTags('informations');
-
-        return $language_id;
-    }
-
-    public function editLanguage($language_id, $data)
-    {
-        $language_query = $this->db->get("SELECT `code` FROM " . DB_PREFIX . "language WHERE language_id = '" . (int)$language_id . "'");
-
-        $this->db->set(
-            DB_PREFIX . 'language',
-            [
-                'name'       => $data['name'],
-                'code'       => $data['code'],
-                'locale'     => $data['locale'],
-                'sort_order' => $data['sort_order'],
-                'status'     => (int)$data['status'],
-            ],
-            ['language_id' => (int)$language_id]
-        );
-
-        if ($language_query->row['code'] != $data['code']) {
-            $this->db->set(
-                DB_PREFIX . 'setting',
-                ['value' => $data['code']],
-                [
-                    'group' => 'system',
-                    'code'  => 'setting',
-                    'key'   => 'language',
-                    'value' => $language_query->row['code'],
-                ]
-            );
-            $this->db->set(
-                DB_PREFIX . 'setting',
-                ['value' => $data['code']],
-                [
-                    'group' => 'system',
-                    'code'  => 'setting',
-                    'key'   => 'admin_language',
-                    'value' => $language_query->row['code'],
-                ]
-            );
-        }
-
-        $this->cache->deleteByTags('languages');
-    }
-
-    public function deleteLanguage($language_id)
-    {
-        $this->db->query("DELETE FROM " . DB_PREFIX . "language WHERE language_id = '" . (int)$language_id . "'");
-
-        $this->cache->deleteByTags('languages');
-
-        $this->db->query("DELETE FROM " . DB_PREFIX . "banner_image_description WHERE language_id = '" . (int)$language_id . "'");
-        $this->db->query("DELETE FROM " . DB_PREFIX . "information_description WHERE language_id = '" . (int)$language_id . "'");
-
-        $this->cache->deleteByTags('informations');
-    }
-
-    public function getLanguage($language_id)
-    {
-        $query = $this->db->get("SELECT DISTINCT * FROM " . DB_PREFIX . "language WHERE language_id = '" . (int)$language_id . "'");
-
-        return $query->row;
-    }
-
-    public function getLanguageByCode($code)
-    {
-        $query = $this->db->get("SELECT * FROM `" . DB_PREFIX . "language` WHERE code = '" . $this->db->escape($code) . "'");
-
-        return $query->row;
-    }
-
-    public function getTotalLanguages()
-    {
-        $query = $this->db->get("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "language");
-
-        return $query->row['total'];
-    }
-    */
 }
