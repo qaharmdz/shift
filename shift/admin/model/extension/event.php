77c0aa1129f5219e5c6dc1dfe01a33572f5abee9
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-10-09 23:03:06 +0700

    Cleanup

--- shift/admin/model/extension/event.php
+++ shift/admin/model/extension/event.php
@@ -9,7 +9,7 @@ use Shift\System\Helper;
 
 class Event extends Mvc\Model
 {
-        /**
+    /**
      * DataTables records
      *
      * @param  array  $params
@@ -64,92 +64,4 @@ class Event extends Mvc\Model
     {
         return (int)$this->db->get("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "event`")->row['total'];
     }
-
-    // Form CRUD
-    // ================================================
-
-    /*
-    public function addEvent($code, $trigger, $action, $status = 1)
-    {
-        $this->db->query("INSERT INTO `" . DB_PREFIX . "event` SET `code` = '" . $this->db->escape($code) . "', `trigger` = '" . $this->db->escape($trigger) . "', `action` = '" . $this->db->escape($action) . "', `status` = '" . (int)$status . "', `date_added` = now()");
-
-        return $this->db->getLastId();
-    }
-
-    public function deleteEvent($code)
-    {
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "event` WHERE `code` = '" . $this->db->escape($code) . "'");
-    }
-
-    public function getEvent($code, $trigger, $action)
-    {
-        $event = $this->db->get("SELECT * FROM `" . DB_PREFIX . "event` WHERE `code` = '" . $this->db->escape($code) . "' AND `trigger` = '" . $this->db->escape($trigger) . "' AND `action` = '" . $this->db->escape($action) . "'");
-
-        return $event->rows;
-    }
-
-    public function enableEvent($event_id)
-    {
-        $this->db->query("UPDATE " . DB_PREFIX . "event SET `status` = '1' WHERE event_id = '" . (int)$event_id . "'");
-    }
-
-    public function disableEvent($event_id)
-    {
-        $this->db->query("UPDATE " . DB_PREFIX . "event SET `status` = '0' WHERE event_id = '" . (int)$event_id . "'");
-    }
-
-    public function uninstall($type, $code)
-    {
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "extension` WHERE `type` = ?s AND `code` = ?s", [$type, $code]);
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE `group` = ?S AND `code` = ?s", [$type, $code]);
-    }
-
-    public function getEvents($data = array())
-    {
-        $sql = "SELECT * FROM `" . DB_PREFIX . "event`";
-
-        $sort_data = array(
-            'group',
-            'trigger',
-            'action',
-            'status',
-            'date_added'
-        );
-
-        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-            $sql .= " ORDER BY `" . $data['sort'] . "`";
-        } else {
-            $sql .= " ORDER BY `group`";
-        }
-
-        if (isset($data['order']) && ($data['order'] == 'DESC')) {
-            $sql .= " DESC";
-        } else {
-            $sql .= " ASC";
-        }
-
-        if (isset($data['start']) || isset($data['limit'])) {
-            if ($data['start'] < 0) {
-                $data['start'] = 0;
-            }
-
-            if ($data['limit'] < 1) {
-                $data['limit'] = 20;
-            }
-
-            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
-        }
-
-        $query = $this->db->get($sql);
-
-        return $query->rows;
-    }
-
-    public function getTotalEvents()
-    {
-        $query = $this->db->get("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "event");
-
-        return $query->row['total'];
-    }
-    */
 }
--- shift/admin/model/tool/layout.php
+++ shift/admin/model/tool/layout.php
@@ -171,37 +171,7 @@ class Layout extends Mvc\Model
 
     public function getLayouts($data = array()): array
     {
-        $sql = "SELECT * FROM " . DB_PREFIX . "layout";
-
-        $sort_data = array('name');
-
-        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-            $sql .= " ORDER BY " . $data['sort'];
-        } else {
-            $sql .= " ORDER BY name";
-        }
-
-        if (isset($data['order']) && ($data['order'] == 'DESC')) {
-            $sql .= " DESC";
-        } else {
-            $sql .= " ASC";
-        }
-
-        if (isset($data['start']) || isset($data['limit'])) {
-            if ($data['start'] < 0) {
-                $data['start'] = 0;
-            }
-
-            if ($data['limit'] < 1) {
-                $data['limit'] = 20;
-            }
-
-            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
-        }
-
-        $query = $this->db->get($sql);
-
-        return $query->rows;
+        return $this->db->get("SELECT * FROM " . DB_PREFIX . "layout ORDER BY name ASC")->rows;
     }
 
     public function getTotal(): int
@@ -217,76 +187,4 @@ class Layout extends Mvc\Model
 
         $this->cache->deleteByTags('layouts');
     }
-
-    /*
-    public function addLayout($data)
-    {
-        $this->db->query("INSERT INTO " . DB_PREFIX . "layout SET name = '" . $this->db->escape($data['name']) . "'");
-
-        $layout_id = $this->db->getLastId();
-
-        if (isset($data['layout_route'])) {
-            foreach ($data['layout_route'] as $layout_route) {
-                $this->db->query("INSERT INTO " . DB_PREFIX . "layout_route SET layout_id = '" . (int)$layout_id . "', site_id = '" . (int)$layout_route['site_id'] . "', route = '" . $this->db->escape($layout_route['route']) . "'");
-            }
-        }
-
-        if (isset($data['layout_module'])) {
-            foreach ($data['layout_module'] as $layout_module) {
-                $this->db->query("INSERT INTO " . DB_PREFIX . "layout_module SET layout_id = '" . (int)$layout_id . "', code = '" . $this->db->escape($layout_module['code']) . "', position = '" . $this->db->escape($layout_module['position']) . "', sort_order = '" . (int)$layout_module['sort_order'] . "'");
-            }
-        }
-
-        return $layout_id;
-    }
-
-    public function editLayout($layout_id, $data)
-    {
-        $this->db->query("UPDATE " . DB_PREFIX . "layout SET name = '" . $this->db->escape($data['name']) . "' WHERE layout_id = '" . (int)$layout_id . "'");
-
-        $this->db->query("DELETE FROM " . DB_PREFIX . "layout_route WHERE layout_id = '" . (int)$layout_id . "'");
-
-        if (isset($data['layout_route'])) {
-            foreach ($data['layout_route'] as $layout_route) {
-                $this->db->query("INSERT INTO " . DB_PREFIX . "layout_route SET layout_id = '" . (int)$layout_id . "', site_id = '" . (int)$layout_route['site_id'] . "', route = '" . $this->db->escape($layout_route['route']) . "'");
-            }
-        }
-
-        $this->db->query("DELETE FROM " . DB_PREFIX . "layout_module WHERE layout_id = '" . (int)$layout_id . "'");
-
-        if (isset($data['layout_module'])) {
-            foreach ($data['layout_module'] as $layout_module) {
-                $this->db->query("INSERT INTO " . DB_PREFIX . "layout_module SET layout_id = '" . (int)$layout_id . "', code = '" . $this->db->escape($layout_module['code']) . "', position = '" . $this->db->escape($layout_module['position']) . "', sort_order = '" . (int)$layout_module['sort_order'] . "'");
-            }
-        }
-    }
-
-    public function getLayout($layout_id)
-    {
-        $query = $this->db->get("SELECT DISTINCT * FROM " . DB_PREFIX . "layout WHERE layout_id = '" . (int)$layout_id . "'");
-
-        return $query->row;
-    }
-
-    public function getLayoutRoutes($layout_id)
-    {
-        $query = $this->db->get("SELECT * FROM " . DB_PREFIX . "layout_route WHERE layout_id = '" . (int)$layout_id . "'");
-
-        return $query->rows;
-    }
-
-    public function getLayoutModules($layout_id)
-    {
-        $query = $this->db->get("SELECT * FROM " . DB_PREFIX . "layout_module WHERE layout_id = '" . (int)$layout_id . "' ORDER BY position ASC, sort_order ASC");
-
-        return $query->rows;
-    }
-
-    public function getTotalLayouts()
-    {
-        $query = $this->db->get("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "layout");
-
-        return $query->row['total'];
-    }
-    */
 }
