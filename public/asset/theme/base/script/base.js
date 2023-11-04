f3b662ec7be1ca33656ed21a28c95c947922e60a
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-11-04 20:08:35 +0700

    Added front header uk-navbar parent icon

---
+++ public/asset/theme/base/script/base.js
@@ -0,0 +1,22 @@
+/* jshint -W097, -W117 */
+/* globals $, document, shift */
+
+'use strict';
+
+if (typeof shift !== 'undefined' && shift.env.development === 1) {
+    console.log(shift);
+}
+
+//=== UIkit components
+UIkit.mixin({
+    data: {
+        offset: 8,
+        mode: 'click',
+        animation: ['uk-animation-slide-bottom-small'],
+    }
+}, 'dropdown');
+UIkit.mixin({
+    data: {
+        animation: ['uk-animation-fade uk-animation-fast']
+    }
+}, 'tab');
