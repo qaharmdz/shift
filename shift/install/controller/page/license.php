2a0cb2376ac067b0742c474a280d0aed653da58f
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2023-11-18 05:49:22 +0700

    Added install/license

---
+++ shift/install/controller/page/license.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shift\Install\Controller\Page;
+
+use Shift\System\Mvc;
+
+class License extends Mvc\Controller
+{
+    public function index()
+    {
+        $this->document->setTitle($this->language->get('license_agreement'));
+
+        $data = [];
+
+
+
+        $this->response->setOutput($this->load->view('page/license', $data));
+    }
+}
--- shift/install/language/en/en.php
+++ shift/install/language/en/en.php
@@ -1,17 +1,18 @@
 <?php
 
-$_['lang_code']         = 'en';
-$_['lang_dir']          = 'ltr';
+$_['lang_code']           = 'en';
+$_['lang_dir']            = 'ltr';
 
-$_['welcome']           = 'Welcome';
-$_['license_agreement'] = 'License Agreement';
-$_['read_agreement']    = 'Please read the following license agreement:';
-$_['detail_agreement']  = 'The details of the GNU General Public License v3 agreement:';
+$_['welcome']             = 'Welcome';
+$_['lets_go']             = 'Let\'s Go!';
+$_['back']                = '< Back';
+$_['agree_continue']      = 'Agree and Continue';
 
-$_['lets_go']           = 'Let\'s Go!';
-$_['back']              = '< Back';
-$_['agree_continue']    = 'Agree and Continue';
+$_['license_agreement']   = 'License Agreement';
+$_['read_agreement']      = 'Please read the following license agreement               : ';
+$_['detail_agreement']    = 'The details of the GNU General Public License v3 agreement: ';
+
 
 // Footer
-$_['copyright']         = '<a href="https://github.com/qaharmdz/shift" target="_blank" rel="noopener noreferrer">Shift CMS</a> &copy; ' . date('Y') . ' All Rights Reserved.';
-$_['version']           = 'Version %s';
+$_['copyright'] = '<a href = "https: //github.com/qaharmdz/shift" target = "_blank" rel = "noopener noreferrer">Shift CMS</a> &copy; ' . date('Y') . ' All Rights Reserved.';
+$_['version']   = 'Version %s';
